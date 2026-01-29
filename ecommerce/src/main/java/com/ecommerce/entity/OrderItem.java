87cd23aaad4abc4ec59f3a6cb4c6c145e339b66d
Aditya Bangar <adityabangar1334@gmail.com>
2026-01-29 12:29:18 +0530

    Initial Spring Boot backend setup

--- ecommerce/src/main/java/com/ecommerce/entity/OrderItem.java
+++ ecommerce/src/main/java/com/ecommerce/entity/OrderItem.java
@@ -1,9 +1,11 @@
 package com.ecommerce.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
+@Table(name = "order_items")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -14,12 +16,18 @@ public class OrderItem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    // MANY ITEMS -> ONE ORDER
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    @JsonIgnoreProperties({"items"})
     private Order order;
 
-    @ManyToOne
+    // MANY ITEMS -> ONE PRODUCT
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "product_id")
     private Product product;
 
     private int quantity;
+
     private double price;
 }
